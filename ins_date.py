9b29c8eed732a2a57dbe2b7ab97632d907551a03
Ryan <rbenson@users.noreply.github.com>
2015-11-11 18:52:04 -0500

    Added more date opts

--- ins_date.py
+++ ins_date.py
@@ -26,13 +26,15 @@ class InsertDatePanelCommand(sublime_plugin.TextCommand):
 
     def run(self, edit):
         #  Set formats
-        d1 = str(datetime.date.today().strftime("%Y-%m-%d"))
-        d2 = str(datetime.date.today().strftime("%B %d, %Y"))
-        d3 = datetime.date.today().strftime("%b-%d-%Y")
-        d4 = datetime.date.today().strftime("%m/%d/%Y")
-        self.list = [d1, d2, d3, d4]
+        d5 = datetime.date.today().strftime("%m/%d/%Y")
+        d6 = datetime.date.today().strftime("%a %m/%d/%Y")
+        d1 = datetime.date.today().strftime("%Y-%m-%d")
+        d2 = datetime.date.today().strftime("%Y-%m-%d %a")
+        d3 = datetime.date.today().strftime("%B %d, %Y")
+        d4 = datetime.date.today().strftime("%b-%d-%Y")
+        self.list = [d5,d6, d2, d3, d4]
 
-        self.view.window().show_quick_panel(self.list, self.on_done,0,1)
+        self.view.window().show_quick_panel(self.list, self.on_done,0,2)
 
 
 class InsertMyText(sublime_plugin.TextCommand):
